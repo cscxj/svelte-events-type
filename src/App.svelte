fd93ea6dff5a2eb4d74c1e4123b3ef429b3e7dcb
cxj <2191505874@qq.com>
2022-07-28 11:03:17 +0800

    init

---
+++ src/App.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import Container from "./Container.svelte";
+</script>
+
+<div>
+  <Container
+    on:hoverContainer={({ detail }) => {
+      console.log(detail);
+    }}
+  />
+</div>
---
+++ src/Container.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  import { createEventDispatcher } from "svelte";
+
+  type Events = {
+    hoverContainer: { point: [number, number] };
+  };
+
+  const dispatch = createEventDispatcher<Events>();
+</script>
